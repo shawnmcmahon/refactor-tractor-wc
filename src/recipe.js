35dff4f711868c968ea67e54936ae90b118a452d
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-06-02 20:43:57 -0600

    Finish recipe methods

--- src/recipe.js
+++ src/recipe.js
@@ -1,17 +1,82 @@
+import {
+  ingredientsData
+} from './data/ingredient-data';
 
 
 class Recipe {
-  constructor(recipe) {
+  constructor(recipe, ingredientsData) {
     this.id = recipe.id;
     this.name = recipe.name;
     this.image = recipe.image;
     this.ingredients = recipe.ingredients;
     this.tags = recipe.tags;
-    this.ingredientNames = []
-    // this.instructions = recipe.instructions;
+    this.instructions = recipe.instructions;
+    this.ingredientsData = ingredientsData
 
   }
+
+  updateIngredientData() {
+    const getIngs = this.ingredientsData.filter(ingredient => ingredient.name && ingredient.id);
+    this.ingredientsData = getIngs
+  }
+
+  returnInstructions() {
+    return this.instructions;
+  }
+
+  returnIngredients() {
+    return this.ingredients
+  }
+
+  getIngredients() {
+   const matchedIngredients = this.ingredients.map(ingredient => {
+     const foundIngredient = this.ingredientsData.find(
+       data => data.id === ingredient.id
+     );
+     const newObj = Object.assign(foundIngredient, ingredient);
+     return newObj
+   });
+   return matchedIngredients;
+ }
+
+ getRecipeCost() {
+     const ingredients = this.getIngredients();
+
+     const total = ingredients.reduce((acc, item) => {
+       acc += item.quantity.amount * item.estimatedCostInCents;
+       return acc;
+     }, 0);
+
+     const costInDollars = (total / 100).toFixed(2);
+     return `$${costInDollars}`;
+   }
+
+  getIngredientNames() {
+      const matchedIngredients = this.ingredients.map(ingredient => {
+        const foundIngredient = this.ingredientsData.find(
+          data => data.id === ingredient.id
+        );
+        const newObj = Object.assign(foundIngredient, ingredient);
+        console.log("obj", newObj)
+        return newObj.name
+      });
+      return matchedIngredients;
+    }
+
+
+
 }
+
+
+
+// A Recipe represents one recipe object.
+//
+// It should hold on to all its information (provided in the data file).
+// It should have methods to:
+// Determine the names of ingredients needed
+// Get the cost of its ingredients
+// Return its directions / instructions
+
 
 //   calculateIngredientsCost() {
 //     // return this.ingredients.map(i => {
--- test/recipe-test.js
+++ test/recipe-test.js
@@ -16,7 +16,9 @@ describe('Recipe', () => {
 
   beforeEach(function() {
     // recipeInfo = data.recipeData[0];
-    recipe = new Recipe(testRecipes[0]);
+    recipe = new Recipe(testRecipes[0], testIngredients);
+    // recipe =
+
   })
 
   it('is a function', function() {
@@ -72,15 +74,150 @@ describe('Recipe', () => {
       "appetizer"
     ]);
   })
-  it.only('should store ingredient names in an array', function() {
-    expect(recipe.ingredientNames).to.be.an('array');
+
+//Need to figure out how to make this test pass since it's not certain what to do with the  filter method in this test.
+  // it('should have access to ingredientsData', function() {
+  //   expect(recipe.ingredientData).to.eq(testIngredients.filter(ingredient => ingredient.name && ingredient.id));
+  // })
+
+
+  it('should store instructions in an array', () => {
+  expect(recipe.instructions).to.be.an('array');
+})
+
+
+  it('should store recipe instructions', () => {
+      expect(recipe.instructions).to.deep.equal([
+        {
+          "instruction": "Cook rice.",
+          "number": 1
+        },
+        {
+          "instruction": "Fry egg.",
+          "number": 2
+        },
+        {
+          "instruction": "Slice avocado.",
+          "number": 3
+        },
+        {
+          "instruction": "Once rice is cooked, scoop out desired portion into a bowl and top with egg and avocado slices. Garnish with chives and lime wedge.",
+          "number": 4
+        }
+      ]);
+    })
+
+  it('should return the recipe instructions', () => {
+          expect(recipe.returnInstructions()).to.deep.equal([
+            {
+              "instruction": "Cook rice.",
+              "number": 1
+            },
+            {
+              "instruction": "Fry egg.",
+              "number": 2
+            },
+            {
+              "instruction": "Slice avocado.",
+              "number": 3
+            },
+            {
+              "instruction": "Once rice is cooked, scoop out desired portion into a bowl and top with egg and avocado slices. Garnish with chives and lime wedge.",
+              "number": 4
+            }
+          ]);
+        })
+
+    it('should return the recipe ingredient details', () => {
+          expect(recipe.returnIngredients()).to.deep.equal([
+            {
+              "id": 0,
+              "quantity": {
+                "amount": 2,
+                "unit": "c"
+              }
+            },
+            {
+              "id": 1,
+              "quantity": {
+                "amount": 1,
+                "unit": "large"
+              }
+            },
+            {
+              "id": 2,
+              "quantity": {
+                "amount": 1,
+                "unit": "large"
+              }
+            }
+          ]);
+        })
+
+
+      it('should start with ingredients Data in an array', () => {
+        expect(recipe.ingredientsData).to.deep.equal(testIngredients);
+})
+
+//This currently works, but i had to edit the this.ingredientData property and moved the methods of matching into a new function.
+//BUT i had to delete the API data name (ingData --> ingredientsData) because our test data doesn't match that, and we need it to match so that we can access the api
+it('should update the ingredient data', () => {
+  const getData = recipe.updateIngredientData(testIngredients)
+  // console.log("test", recipe.ingredientsData)
+  const ingredientInfo = [
+  { id: 1, name: 'egg', estimatedCostInCents: 10 },
+  { id: 2, name: 'avocado', estimatedCostInCents: 250 },
+  { id: 3, name: 'tomatillo', estimatedCostInCents: 50 },
+  { id: 4, name: 'garlic', estimatedCostInCents: 25 },
+  { id: 5, name: 'jalapeno', estimatedCostInCents: 10 },
+  { id: 6, name: 'cilantro', estimatedCostInCents: 50 },
+  { id: 7, name: 'soy sauce', estimatedCostInCents: 5 },
+  { id: 8, name: 'brown sugar', estimatedCostInCents: 10 }
+]
+  expect(recipe.ingredientsData).to.deep.equal(ingredientInfo);
+})
+
+it('should have a way to return all ingredient info needed', () => {
+  const ingredients = recipe.getIngredients();
+  // console.log("Test1", ingredients)
+  const answer = [
+  {
+    id: 0,
+    name: "rice",
+    estimatedCostInCents: 150,
+    quantity: {amount: 2, unit: 'c'}
+  },
+  {
+    id: 1,
+    name: "egg",
+    estimatedCostInCents: 10,
+    quantity: {amount: 1, unit: "large"}
+  },
+  {
+    id: 2,
+    name: "avocado",
+    estimatedCostInCents: 250,
+    quantity: {amount: 1, unit: "large"}
+  }
+]
+// console.log("Test2", ingredients)
+  expect(ingredients).to.deep.equal(answer)
+})
+
+it('should be able to get the cost of the ingredients', () => {
+   const cost = recipe.getRecipeCost();
+   console.log("cost", cost)
+   const answer = "$5.60"
+
+   expect(cost).to.equal(answer)
+ })
+
+ it.only('should be able to get the names of the ingredients', () => {
+    const getNames = recipe.getIngredientNames();
+
+    console.log(getNames)
+    expect(getNames).to.deep.equal([ 'rice', 'egg', 'avocado' ])
   })
 
-  it('should start with no ingredient names in an array', function() {
-    expect(recipe.ingredientNames).to.deep.equal([]);
-  })
 
-  it('should calculate the total cost of all of the ingredients', function() {
-    expect(recipe.calculateIngredientsCost()).to.eq();
-  });
 });
