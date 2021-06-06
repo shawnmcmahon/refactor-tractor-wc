17cd66a4a835650c1ddfe38380f5a87c80ee2691
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-06-06 16:38:13 -0600

    Fix issue with home button not working when searching

--- src/scripts.js
+++ src/scripts.js
@@ -36,7 +36,8 @@ let showPantryRecipes = document.getElementById('whatCanIMake');
 let searchForm = document.getElementById('searchBar');
 // let exitBtn = document.getElementById('exit-recipe-btn')
 let recipeSection = document.getElementById('fullRecipeInstructions')
-
+let homeBtn = document.getElementById('myHomeButton')
+let allRecipeCards = document.getElementById('allRecipeCards');
 // variables
 let user, cookbook, pantry;
 let globalIngredientsData = {};
@@ -51,8 +52,10 @@ window.addEventListener('click', () => clickRecipeCard(event));
 searchForm.addEventListener('submit', () =>  pressEnterSearch(event));
 searchBtn.addEventListener('click', searchRecipes);
 recipeSection.addEventListener('click', domUpdates.exitRecipe);
-allRecipesBtn.addEventListener('click', () => domUpdates.renderRecipeCards(cookbook))
+allRecipesBtn.addEventListener('click', () => domUpdates.renderRecipeCards(cookbook, user))
 allRecipesBtn.addEventListener('click', domUpdates.toggleBannerText)
+homeBtn.addEventListener('click', () => domUpdates.renderRecipeCards(cookbook, user))
+allRecipeCards.addEventListener('click', domUpdates.exitRecipe);
 // exitBtn.addEventListener('click', domUpdates.exitRecipe)
 // main.addEventListener('click', addToMyRecipes);
 // allRecipesBtn.addEventListener('click', showAllRecipes);
@@ -67,7 +70,7 @@ function startUp() {
       globalIngredientsData = promise[2].ingredientsData
       domUpdates.updateWelcomeMessage(user);
       getTagsFromRecipeData()
-      domUpdates.renderRecipeCards(cookbook)
+      domUpdates.renderRecipeCards(cookbook, user)
       domUpdates.displayPantryInfo(pantry)
     })
 
