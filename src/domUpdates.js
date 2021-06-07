cdd528ffc760d409202fac5c51bf4e7f26ae8b4d
Claire Fields <clairefields15@gmail.com>
2021-06-06 19:09:56 -0600

    Remove old bad code and comments

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 let fullRecipeInfo;
 
 let domUpdates = {
@@ -19,9 +20,7 @@ let domUpdates = {
   },
 
   renderRecipeCards(cookbook, user) {
-    user.viewHome()
-    // user.isViewingFavorites = false;
-    // user.isViewingRecipesToCook = false;
+    user.viewHome();
     let allRecipeCards = document.getElementById('allRecipeCards');
     let recipes = cookbook.cookbook;
     allRecipeCards.innerHTML = '';
@@ -37,50 +36,6 @@ let domUpdates = {
           <h4>${recipe.tags[0]}</h4>
           <img src='../images/add-to-cook-queue-2.png' id=${recipe.id} alt="add to cook queue icon" class='card-silverware-icon'>
           <img src='../images/apple-logo-outline.png' id=${recipe.id} alt='unfilled apple icon' class='card-apple-icon'>
-        </div>`;
-      allRecipeCards.insertAdjacentHTML('beforeend', cardHtml);
-    });
-  },
-
-  renderFavoriteRecipeCards(user) {
-    let allRecipeCards = document.getElementById('allRecipeCards');
-    let recipes = user.favoriteRecipes;
-    // allRecipeCards.innerHTML = '';
-    recipes.forEach(recipe => {
-      let name = domUpdates.shortenNames(recipe);
-
-      let cardHtml = `
-        <div class='recipe-card' id=${recipe.id}>
-          <h3>${name}</h3>
-          <div class='card-photo-container'>
-            <img src=${recipe.image} class='card-photo-preview' alt='${recipe.name} recipe' title='${recipe.name} recipe'>
-          </div>
-          <h4>${recipe.tags[0]}</h4>
-          <img src='../images/add-to-cook-queue-2.png' id=${recipe.id} alt="add to cook queue icon" class='card-silverware-icon'>
-          <img src='../images/apple-logo-outline.png' id=${recipe.id} alt='unfilled apple icon' class='card-apple-icon'>
-        </div>`;
-      allRecipeCards.insertAdjacentHTML('beforeend', cardHtml);
-    });
-  },
-
-  // this isn't called anywhere??
-  renderRecipeToCookCards(user) {
-    user.viewCookBook();
-    let allRecipeCards = document.getElementById('allRecipeCards');
-    let recipes = user.recipesToCook;
-    // allRecipeCards.innerHTML = '';
-    recipes.forEach(recipe => {
-      let name = domUpdates.shortenNames(recipe);
-
-      let cardHtml = `
-        <div class='recipe-card' id=${recipe.id}>
-          <h3>${name}</h3>
-          <div class='card-photo-container'>
-            <img src='${recipe.image}' id=${recipe.id} class='card-photo-preview' alt='${recipe.name} recipe' title='${recipe.name} recipe'>
-          </div>
-          <h4>${recipe.tags[0]}</h4>
-          <img src='../images/add-to-cook-queue-2.png' id=${recipe.id} alt="add to cook queue icon" class='card-silverware-icon'>
-          <img src='../images/apple-logo-outline.png' id=${recipe.id}  alt='unfilled apple icon' class='card-apple-icon'>
         </div>`;
       allRecipeCards.insertAdjacentHTML('beforeend', cardHtml);
     });
@@ -108,7 +63,6 @@ let domUpdates = {
     });
   },
 
-  // we could potentially delete this function, I refactored it but I assume it had something to do with styling.
   shortenNames(recipe) {
     if (recipe.name.length > 40) {
       return recipe.name.substring(0, 40) + '...';
@@ -130,10 +84,6 @@ let domUpdates = {
     }
   },
 
-  // this function needs info out of pantry class
-  // once we have the names, we can render the pantry the DOM with all the items in our pantry
-  // add a + and - button next to each one to do the post request
-  // we don't actually want a checkbox, we want a plus and minus and the amount ex: " - 2 + "
   displayPantryInfo(pantry) {
     let updatePantryIngs = pantry.returnPantryIngredients();
     updatePantryIngs.forEach(ingredient => {
@@ -153,6 +103,10 @@ let domUpdates = {
     });
   },
 
+  // change this modify it????
+  // every time you click home you want to run domUpdates.updateWelcomeMessage(user)
+  //every time you click hat you want to see my favorites
+  // every time you click cook you want to see my cook list
   toggleBannerText() {
     let welcomeMessage = document.getElementById('welcomeMessage');
     let myRecipesBanner = document.getElementById('myRecipesBanner');
@@ -163,8 +117,7 @@ let domUpdates = {
   openRecipeInfo(recipe) {
     allRecipeCards.innerHTML += `
     <div class="recipe-instructions" id="fullRecipeInstructions">
-    `
-
+    `;
     fullRecipeInfo = document.getElementById('fullRecipeInstructions');
     fullRecipeInfo.style.display = 'inline';
     domUpdates.generateRecipeTitle(recipe);
@@ -236,7 +189,6 @@ let domUpdates = {
 
   exitRecipe(event) {
     let overlay = document.getElementById('overlay');
-
     if (event.target.id === 'exit-recipe-btn') {
       while (
         fullRecipeInfo.firstChild &&
@@ -246,54 +198,7 @@ let domUpdates = {
         overlay.remove();
       }
     }
-  },
-
-  showSavedRecipes() {
-    let unsavedRecipes = recipes.filter(recipe => {
-      return !user.favoriteRecipes.includes(recipe.id);
-    });
-    unsavedRecipes.forEach(recipe => {
-      let domRecipe = document.getElementById(`${recipe.id}`);
-      domRecipe.style.display = 'none';
-    });
-  },
-
-  ///////////// everything above this line is not total garbage /////////////
-
-  //This could definitely be broken down for more SRP code
-  addToMyRecipes() {
-    if (event.target.className === 'card-apple-icon') {
-      let cardId = parseInt(event.target.closest('.recipe-card').id);
-      if (!user.favoriteRecipes.includes(cardId)) {
-        event.target.src = '../images/apple-logo.png';
-        user.saveRecipe(cardId);
-      } else {
-        event.target.src = '../images/apple-logo-outline.png';
-        user.removeRecipe(cardId);
-      }
-    } else if (event.target.id === 'exit-recipe-btn') {
-      exitRecipe();
-    } else if (
-      isDescendant(event.target.closest('.recipe-card'), event.target)
-    ) {
-      openRecipeInfo(event);
-    }
-  },
-
-  // addToRecipesToCook() {
-  //   if (event.target.className === 'card-silverware-icon') {
-  //     let cardId = parseInt(event.target.closest('.recipe-card').id);
-  //     if (!user.recipesToCook.includes(cardId)) {
-  //       event.target.src = '../images/remove-from-cook-queue.png';
-  //       user.decideToCook(cardId);
-  //     } else {
-  //       event.target.src = '../images/add-to-cook-queue-2.png';
-  //       user.removeFromRecipesToCook(cardId);
-  //       console.log(user.recipesToCook)
-  //     }
-  //   }
-  //
-  // },
-}
+  }
+};
 
 export default domUpdates
--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,4 @@
 /* eslint-disable max-len */
-//imports
-// import './css/base.scss';
-// import './css/styles.scss';
-
 import './css/index.scss';
 
 import domUpdates from './domUpdates';
@@ -30,37 +26,24 @@ let pantryBtn = document.getElementById('myPantryButton');
 let savedRecipesBtn = document.getElementById('myFavRecipesButton');
 let searchBtn = document.getElementById('searchButton');
 let searchInput = document.getElementById('searchInput');
-let allRecipesBtn = document.getElementById('showAllRecipesButton');
-let showPantryRecipes = document.getElementById('whatCanIMake');
 let searchForm = document.getElementById('searchBar');
 let filterRecipesBtn = document.getElementById('filterRecipesButton')
-// let exitBtn = document.getElementById('exit-recipe-btn')
-let tagList = document.getElementById('tagList')
-let recipeSection = document.getElementById('fullRecipeInstructions')
 let homeBtn = document.getElementById('myHomeButton')
 let allRecipeCards = document.getElementById('allRecipeCards');
+
 // variables
 let user, cookbook, pantry;
-let globalIngredientsData = {};
 
 //event listeners
 window.onload = startUp()
 filterRecipesBtn.addEventListener('click', findCheckedTags);
-//showPantryRecipes.addEventListener('click', domUpdates.findCheckedPantryBoxes);
 pantryBtn.addEventListener('click', domUpdates.togglePantryMenu);
 savedRecipesBtn.addEventListener('click', findFavoriteRecipes);
 window.addEventListener('click', () => clickRecipeCard(event));
 searchForm.addEventListener('submit', () =>  pressEnterSearch(event));
 searchBtn.addEventListener('click', searchRecipes);
-recipeSection.addEventListener('click', domUpdates.exitRecipe);
-allRecipesBtn.addEventListener('click', () => domUpdates.renderRecipeCards(cookbook, user))
-allRecipesBtn.addEventListener('click', domUpdates.toggleBannerText)
 homeBtn.addEventListener('click', () => domUpdates.renderRecipeCards(cookbook, user))
 allRecipeCards.addEventListener('click', domUpdates.exitRecipe);
-// exitBtn.addEventListener('click', domUpdates.exitRecipe)
-// main.addEventListener('click', addToMyRecipes);
-// allRecipesBtn.addEventListener('click', showAllRecipes);
-// showPantryRecipes.addEventListener('click', findCheckedPantryBoxes);
 
 function startUp() {
   apiCalls.retrieveData()
@@ -68,7 +51,6 @@ function startUp() {
       makeUserInstance(promise[0].usersData, promise[2].ingredientsData);
       const allRecipes = makeRecipeInstances(promise[1].recipeData, promise[2].ingredientsData);
       cookbook = new Cookbook(allRecipes, promise[2].ingredientsData);
-      globalIngredientsData = promise[2].ingredientsData
       domUpdates.updateWelcomeMessage(user);
       getTagsFromRecipeData()
       domUpdates.renderRecipeCards(cookbook, user)
@@ -86,7 +68,6 @@ function makeUserInstance(apiUserData, apiIngredientData) {
 function makePantryInstance(user, apiIngredientData) {
   pantry = new Pantry(user, apiIngredientData)
 }
-
 
 function makeRecipeInstances(apiRecipeData, apiIngredientData) {
   const newRecipes = apiRecipeData.map(recipe => {
@@ -122,29 +103,24 @@ function clickRecipeCard(event) {
     const foundRecipe = cookbook.cookbook.find(
       recipe => recipeId === recipe.id
     );
-      if(!user.favoriteRecipes.includes(foundRecipe)) {
-        user.saveRecipe(foundRecipe);
-        console.log("Faves", user.favoriteRecipes)
-      }
-  } else if(eventSilverwareTarget) {
+    if(!user.favoriteRecipes.includes(foundRecipe)) {
+      user.saveRecipe(foundRecipe);
+    }
+  } else if (eventSilverwareTarget) {
     let silverWareId = parseInt(eventSilverwareTarget.id);
     const foundRecipe = cookbook.cookbook.find(
       recipe => silverWareId === recipe.id
     );
-    if(!user.recipesToCook.includes(foundRecipe)) {
+    if (!user.recipesToCook.includes(foundRecipe)) {
       user.decideToCook(foundRecipe);
-      console.log("toCook", user.recipesToCook)
     }
   }
 }
-
 
 function findFavoriteRecipes() {
   let favorites = user.favoriteRecipes
   domUpdates.renderSearchResults(favorites)
   //need to get banner text to show when you are viewing favorites
-  domUpdates.toggleBannerText()
-
 }
 
 function pressEnterSearch(event) {
@@ -156,32 +132,7 @@ function searchRecipes() {
   let input = searchInput.value
   let results = cookbook.filterByNameOrIngredient(input);
   domUpdates.renderSearchResults(results)
-  domUpdates.toggleBannerText()
   searchInput.value = ''
-}
-
-
-///////////// everything above this line is not total garbage /////////////
-
-
-//this is for pantry class
-// this function is fired off inside of findCheckedPantryBoxes
-// selected is an array of pantryCheckboxes (whatever that means)
-function findRecipesWithCheckedIngredients(selected) {
-  let recipeChecker = (arr, target) => target.every(v => arr.includes(v));
-  let ingredientNames = selected.map(item => {
-    return item.id;
-  });
-  recipes.forEach(recipe => {
-    let allRecipeIngredients = [];
-    recipe.ingredients.forEach(ingredient => {
-      allRecipeIngredients.push(ingredient.name);
-    });
-    if (!recipeChecker(allRecipeIngredients, ingredientNames)) {
-      let domRecipe = document.getElementById(`${recipe.id}`);
-      domRecipe.style.display = 'none';
-    }
-  });
 }
 
 function findCheckedTags() {
@@ -193,286 +144,3 @@ function findCheckedTags() {
   let results = cookbook.filterByTag(selectedTags);
   domUpdates.renderSearchResults(results)
 }
-
-
-//This might not be needed if we can find a way that html just generates the recipes that have been searched
-function filterNonSearched(filtered) {
-  let found = recipes.filter(recipe => {
-    let ids = filtered.map(f => f.id);
-    return !ids.includes(recipe.id)
-  })
-  hideUnselectedRecipes(found);
-}
-
-
-//dont think this is fully necessary, could probably just instantiate a pantry on load
-// seems like a really convoluted way to
-// function findPantryInfo() {
-//   //seems like a weird way to match ids and ingredients, should go in recipe class maybe?
-//   // in any case, ingredientsData is hardcoded atm and we will fetch
-//   user.pantry.forEach(item => {
-//     let itemInfo = ingredientsData.find(ingredient => {
-//       return ingredient.id === item.ingredient;
-//     });
-//     let originalIngredient = pantryInfo.find(ingredient => {
-//       if (itemInfo) {
-//         return ingredient.name === itemInfo.name;
-//       }
-//     });
-//     if (itemInfo && originalIngredient) {
-//       originalIngredient.count += item.amount;
-//     } else if (itemInfo) {
-//       pantryInfo.push({name: itemInfo.name, count: item.amount});
-//     }
-//   });
-//   // wtf is localeCompare
-//   // why would we need to sort pantry
-//   displayPantryInfo(pantryInfo.sort((a, b) => a.name.localeCompare(b.name)));
-// }
-
-
-
-
-
-///////////////// OLD GARBAGE HERE vvv
-
-// FILTER BY RECIPE TAGS
-// Belongs in CookBook.
-// Moved to cookbook
-// function findTags() {
-//   let tags = [];
-//   recipeData.forEach(recipe => {
-//     recipe.tags.forEach(tag => {
-//       if (!tags.includes(tag)) {
-//         tags.push(tag);
-//       }
-//     });
-//   });
-//   tags.sort();
-//   listTags(tags);
-// }
-
-
-
-// Move to dom Updates
-  // let welcomeMsg = `
-  //   <div class='welcome-msg'>
-  //     <h1>Welcome ${firstName}!</h1>
-  //   </div>`;
-  // document.querySelector('.banner-image').insertAdjacentHTML('afterbegin',
-  //   welcomeMsg);
-
-
-//this is the filter by tags that should go in Cookbook.js
-// renamed filterByTag!!!!
-// function filterByTag(selected) {
-//   let filteredResults = [];
-//   selected.forEach(tag => {
-//     let allRecipes = recipes.filter(recipe => {
-//       return recipe.tags.includes(tag.id);
-//     });
-//     allRecipes.forEach(recipe => {
-//       if (!filteredResults.includes(recipe)) {
-//         filteredResults.push(recipe);
-//       }
-//     })
-//   });
-
-
-// this used to be called by showAllRecipes(), it's probably not needed
-//
-// function filterRecipes(filtered) {
-//   let foundRecipes = recipes.filter(recipe => {
-//     return !filtered.includes(recipe);
-//   });
-//   //this should go to domUpdates
-//   hideUnselectedRecipes(foundRecipes)
-// }
-
-//this should go in domUpdates
-//This could potentially be deleted if not necessary
-//
-// function hideUnselectedRecipes(foundRecipes) {
-//   foundRecipes.forEach(recipe => {
-//     let domRecipe = document.getElementById(`${recipe.id}`);
-//     domRecipe.style.display = 'none';
-//   });
-// }
-
-// FAVORITE RECIPE FUNCTIONALITY
-
-//This should go in the User class method (add to my favorites)
-//This could definitely be broken down for more SRP code
-function addToMyRecipes() {
-  if (event.target.className === 'card-apple-icon') {
-    let cardId = parseInt(event.target.closest('.recipe-card').id)
-    if (!user.favoriteRecipes.includes(cardId)) {
-      event.target.src = '../images/apple-logo.png';
-      user.saveRecipe(cardId);
-    } else {
-      event.target.src = '../images/apple-logo-outline.png';
-      user.removeRecipe(cardId);
-    }
-  }
-
-  else if (event.target.id === 'exit-recipe-btn') {
-    exitRecipe();
-  } else if (isDescendant(event.target.closest('.recipe-card'), event.target)) {
-    openRecipeInfo(event);
-  }
-}
-
-//Is this domUpdates?
-//delete this?? It is used in addToMyRecipes which is in User
-// function isDescendant(parent, child) {
-//   let node = child;
-//   while (node !== null) {
-//     if (node === parent) {
-//       return true;
-//     }
-//     node = node.parentNode;
-//   }
-//   return false;
-// }
-
-//This is part User.js (filterFavoriteRecipes) and part domUpdates to show those recipes
-// function showSavedRecipes() {
-//   let unsavedRecipes = recipes.filter(recipe => {
-//     return !user.favoriteRecipes.includes(recipe.id);
-//   });
-//   unsavedRecipes.forEach(recipe => {
-//     let domRecipe = document.getElementById(`${recipe.id}`);
-//     domRecipe.style.display = 'none';
-//   });
-//   showMyRecipesBanner();
-// }
-
-// CREATE RECIPE INSTRUCTIONS
-
-//Remove CSS styling from JS?
-//domUpdates function
-// function openRecipeInfo(event) {
-//   fullRecipeInfo.style.display = 'inline';
-//   let recipeId = event.path.find(e => e.id).id;
-//   let recipe = recipeData.find(recipe => recipe.id === Number(recipeId));
-//   generateRecipeTitle(recipe, generateIngredients(recipe));
-//   addRecipeImage(recipe);
-//   generateInstructions(recipe);
-//   fullRecipeInfo.insertAdjacentHTML('beforebegin', '<section id='overlay'></div>');
-// }
-
-//This should go in the domUpdates
-// function generateRecipeTitle(recipe, ingredients) {
-//   let recipeTitle = `
-//     <button id='exit-recipe-btn'>X</button>
-//     <h3 id='recipe-title'>${recipe.name}</h3>
-//     <h4>Ingredients</h4>
-//     <p>${ingredients}</p>`
-//   fullRecipeInfo.insertAdjacentHTML('beforeend', recipeTitle);
-// }
-
-//This should go in the domUpdates
-// function addRecipeImage(recipe) {
-//   document.getElementById('recipe-title').style.backgroundImage = `url(${recipe.image})`;
-// }
-
-
-// //this should go in Recipe.js (returnIngredients)
-// function generateIngredients(recipe) {
-//   return recipe && recipe.ingredients.map(i => {
-//     return `${capitalize(i.name)} (${i.quantity.amount} ${i.quantity.unit})`
-//   }).join(', ');
-// }
-
-//This should go in Recipe.js (return Instructions) for the first half
-//This bottom part should be in domUpdates
-// function generateInstructions(recipe) {
-//   let instructionsList = '';
-//   let instructions = recipe.instructions.map(i => {
-//     return i.instruction
-//   });
-//   instructions.forEach(i => {
-//     instructionsList += `<li>${i}</li>`
-//   });
-//   fullRecipeInfo.insertAdjacentHTML('beforeend', '<h4>Instructions</h4>');
-//   fullRecipeInfo.insertAdjacentHTML('beforeend', `<ol>${instructionsList}</ol>`);
-// }
-
-//This should go to domUpdates
-// function exitRecipe() {
-//   while (fullRecipeInfo.firstChild &&
-//     fullRecipeInfo.removeChild(fullRecipeInfo.firstChild));
-//   fullRecipeInfo.style.display = 'none';
-//   document.getElementById('overlay').remove();
-// }
-
-// TOGGLE DISPLAYS
-
-// //This goes in domUpdates
-// function showMyRecipesBanner() {
-//   document.querySelector('.welcome-msg').style.display = 'none';
-//   document.querySelector('.my-recipes-banner').style.display = 'block';
-// }
-//
-// // Belongs in domUpdates
-// function showWelcomeBanner() {
-//   document.querySelector('.welcome-msg').style.display = 'flex';
-//   document.querySelector('.my-recipes-banner').style.display = 'none';
-// }
-
-
-
-// Belongs in domUpdates
-//  Is this needed? Is there a better way to do this?
-// function toggleMenu() {
-//   var menuDropdown = document.querySelector('.drop-menu');
-//   menuOpen = !menuOpen;
-//   if (menuOpen) {
-//     menuDropdown.style.display = 'block';
-//   } else {
-//     menuDropdown.style.display = 'none';
-//   }
-// }
-
-//dom manipulation
-// seems an odd way to display recipes
-// function showAllRecipes() {
-//   recipes.forEach(recipe => {
-//     let domRecipe = document.getElementById(`${recipe.id}`);
-//     domRecipe.style.display = 'block';
-//   });
-//   showWelcomeBanner();
-// }
-
-// CREATE AND USE PANTRY
-
-
-// Belongs in domUpdates
-// function displayPantryInfo(pantry) {
-//   pantry.forEach(ingredient => {
-//     let ingredientHtml = `<li><input type='checkbox' class='pantry-checkbox' id='${ingredient.name}'>
-//       <label for='${ingredient.name}'>${ingredient.name}, ${ingredient.count}</label></li>`;
-//     document.querySelector('.pantry-list').insertAdjacentHTML('beforeend',
-//       ingredientHtml);
-//   });
-// }
-
-
-//is called inside findCheckedPantryBoxes
-//probably goes in cookbook class
-// function findRecipesWithCheckedIngredients(selected) {
-//   let recipeChecker = (arr, target) => target.every(v => arr.includes(v));
-//   let ingredientNames = selected.map(item => {
-//     return item.id;
-//   })
-//   recipes.forEach(recipe => {
-//     let allRecipeIngredients = [];
-//     recipe.ingredients.forEach(ingredient => {
-//       allRecipeIngredients.push(ingredient.name);
-//     });
-//     if (!recipeChecker(allRecipeIngredients, ingredientNames)) {
-//       let domRecipe = document.getElementById(`${recipe.id}`);
-//       domRecipe.style.display = 'none';
-//     }
-//   })
-// }
