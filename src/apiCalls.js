a0eff3287b04724368527be17b5736cebb8b1d6d
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-06-08 16:20:43 -0600

    Fix issue with pantry not clearing html before it renders post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,3 @@
-// import modifyPantry from './scripts'
-
 const fetchUserData = () => {
   return fetch('http://localhost:3001/api/v1/users')
     .then(response => checkForError(response))
@@ -27,16 +25,6 @@ const checkForError = (response) => {
   }
 }
 
-// const handleError = (response) => {
-//   if (response.status === 422) {
-//     //display error below empty fields
-//     errorMessage.innerText = 'Error, please fill in any empty fields'
-//   }
-//   if (response.status === 404) {
-//     //display 'Page not found' on screen)
-//     errorMessage.innerText = 'Sorry, page not found... 404';
-//   }
-// }
 
 function addOrRemoveIngredient(userID, ingredientID, ingredientMod, user) {
   return fetch("http://localhost:3001/api/v1/users", {
